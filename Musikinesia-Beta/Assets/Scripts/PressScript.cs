94a18fafc557ac3db9e35966f9caa1bc0d224a1e
PabloFreitas <pabloagfreitas@gmail.com>
2014-05-21 15:20:22 -0300

    Versão Alpha 0.1

---
+++ Musikinesia-Beta/Assets/Scripts/PressScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressScript : MonoBehaviour {
+
+
+	Animator animator;
+	public string key_1 = "a";
+	void Start () {
+		animator = transform.GetComponent<Animator>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (key_1))
+			PlayNote ();
+		if (Input.GetKeyUp (key_1))
+			animator.SetTrigger ("UnPress");
+	}
+
+	void OnMouseDown ()
+	{
+			PlayNote();
+	}
+
+	void OnMouseUp ()
+	{
+		animator.SetTrigger ("UnPress");
+	}
+
+	
+	void PlayNote()
+	{
+		audio.Play ();
+		animator.SetTrigger ("DoPress");
+
+	}			
+}
